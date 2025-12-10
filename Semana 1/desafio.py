2c430b3723c3042cca6fcfc65488aed390ae45f3
Gabriel Malachias Hangrad <607.gmalachias@rte-intra.com.br>
2025-12-10 17:12:01 -0300

    Create desafio.py

---
+++ Semana 1/desafio.py
@@ -0,0 +1,28 @@
+"""
+🧮 Mini Calculadora Inteligente com Relatório
+
+Desenvolva um programa que:
+
+1. Peça ao usuário:
+
+   * Nome
+   * Idade
+   * Dois números
+2. Realize automaticamente as operações:
+
+   * Soma
+   * Subtração
+   * Multiplicação
+   * Divisão
+   * Resto
+   * Expoente
+3. Gere um **relatório final** bem formatado com todos os resultados.
+4. Todos os textos do relatório devem usar **f-strings**.
+5. Trate divisão por zero usando `try/except`.
+6. Mostre ao final:
+
+   * Tipo de cada dado recebido
+   * Data e horário atual (sem módulos externos)
+
+Esse desafio força a usar tudo da Semana 1.
+"""
